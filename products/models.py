736ed383c972a2a645f070924840dd0fef3d945c
deepenkumal <deepenkumal@noreply.github.com>
2021-05-10 10:27:22 +0545

    products models created

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length=150,verbose_name='Product Name')
+    price = models.FloatField()
+    discount_price = models.FloatField()
+    category = models.CharField(max_length=150,verbose_name='Product Category')
+    description = models.TextField(verbose_name='Product Description')
+    image = models.CharField(max_length=150)
