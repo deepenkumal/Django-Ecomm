3359f5938ba564cf68b94e0a652c23838c61040a
deepenkumal <deepenkumal@noreply.github.com>
2021-05-10 11:31:52 +0545

    first list and detail views of porduct

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
+app_name='products'
 urlpatterns = [
     path('',views.index , name='index'),
+    path('product/<str:pk>/detail',views.ProductDetail.as_view(),name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from django.views import generic
 
 # Create your views here.
 def index(request):
-    return render(request,'products/index.html')
+    products = Product.objects.all()
+    return render (request,'products/index.html',{'products':products})
+
+
+class ProductDetail(generic.DetailView):
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+    def get_queryset(self):
+        return Product.objects.all()
+    
